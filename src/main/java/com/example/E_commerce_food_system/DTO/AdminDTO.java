139ab7c27335ef756d35b58b2912ca261fec6332
Ming Hour <minghour648@gmail.com>
2026-06-09 23:50:31 +0700

    My changes

---
+++ src/main/java/com/example/E_commerce_food_system/DTO/AdminDTO.java
@@ -0,0 +1,38 @@
+package com.example.E_commerce_food_system.DTO;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="admin")
+public class AdminDTO {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="admin_id")
+    private Integer adminId;
+
+    @Column(name="full_name", nullable=false)
+    private String fullName;
+
+    @Column(name="email", nullable = false)
+    private String email;
+
+    @Column(name="password", nullable= false)
+    private String password;
+
+    @Column(name= "created_at")
+    private LocalDateTime createdAt;
+
+    public Integer getAdminId() { return adminId; }
+    public void setAdminId(Integer adminId) { this.adminId = adminId; }
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
